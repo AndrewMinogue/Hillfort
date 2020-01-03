bf0e27bf7c5c85e51e775d5df024de75ec3faae0
AndrewMinogue <a.minogue@hotmail.com>
2020-01-03 16:35:22 +0000

    Fix in UP implementation which involved refactoring of Android Manifest file

--- app/src/main/java/assignment/hillfort/views/hillfort/favourite/FavouriteView.kt
+++ app/src/main/java/assignment/hillfort/views/hillfort/favourite/FavouriteView.kt
@@ -2,9 +2,12 @@ package assignment.hillfort.views.hillfort.favourite
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import assignment.hillfort.R
 import assignment.hillfort.main.MainApp
+import assignment.hillfort.views.hillfort.hillfortlist.HillfortListPresenter
 import kotlinx.android.synthetic.main.activity_hillfort.*
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
@@ -16,10 +19,10 @@ class FavouriteView : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings)
+        setContentView(R.layout.activity_hillfort_list)
         app = application as MainApp
-        toolbarAdd.title = title
-        setSupportActionBar(toolbarAdd)
-        info("Hillfort Activity started..")
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
     }
 }
